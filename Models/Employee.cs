7e161e36e2cd8fc17e2c21b560d3d92de32e495a
Mantvydas Šemeta <mantvydassemeta@gmail.com>
2024-07-29 14:26:50 +0300

    Korekcijos

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -13,13 +13,13 @@ namespace Darbuotojai.Models
         public string Name { get; set; }
         public int Age { get; set; }
 
-        public readonly string EmployeesFileLoction;
+      
 
         public Employee (string name, int age)
         {
             Name = name;
             Age = age;
-            EmployeesFileLoction = "C:\\Users\\mantv\\source\\repos\\Darbuotojai\\employees.txt";
+            
         }
 
         public virtual void ReadData()
